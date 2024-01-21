3f6a0dab6996785fd8493693fc4019c42d64af5e
ikad <tokunaga.c.daigo@forgxisto.com>
2024-01-21 11:44:53 +0900

    refactor

--- lib/lookbook_storybook_json_generator/storybook_json_generator.rb
+++ lib/lookbook_storybook_json_generator/storybook_json_generator.rb
@@ -3,14 +3,35 @@
 module LookbookStorybookJsonGenerator
   # lookbook のコードオブジェクトを storybook の json として出力する
   class StorybookJsonGenerator
-    def self.execute(code_objects, _args)
-      code_objects.each do |code_object|
-        # code_object と同じ場所にファイルを作成する
-        # ただし、ファイル名は .stories.json とする
-        file_name = code_object.file.to_s.gsub('.rb', '.stories.json')
-        json = LookbookStorybookJsonGenerator::LookbookCodeObjectParser.new(code_object).to_json
-        File.open(file_name, 'w') { _1.puts json }
-      end
+    def self.execute(code_objects, changes = nil)
+      new(code_objects, changes).execute
+    rescue => e
+      Rails.loggoer.warn "LookbookStorybookJsonGenerator #{e.message}"
+    end
+
+    def initialize(code_objects, changes)
+      @code_objects = code_objects
+      @changes = changes
+    end
+
+    def execute
+      generate_all
+    end
+
+    private
+
+    attr_reader :code_objects, :changes
+
+    def generate_all
+      code_objects.each { generate_json(_1) }
+    end
+
+    # code_object と同じ場所にファイルを作成する
+    # ただし、ファイル名は .stories.json とする
+    def generate_json(code_object)
+      file_name = code_object.file.to_s.gsub('.rb', '.stories.json')
+      json = LookbookStorybookJsonGenerator::LookbookCodeObjectParser.new(code_object).to_json
+      File.open(file_name, 'w') { _1.puts json }
     end
   end
 end
